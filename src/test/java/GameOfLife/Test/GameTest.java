76b2dce924768e86f142f851e35bc91b1df4846f
Chris_Gornall <chris.gornall@gmail.com>
2015-03-03 14:04:12 +0000

    Revert "adding second test, board class and overide methods"
    This reverts commit dd4d4adc7b63020ef778a1bfd5a98f183b5d607e.

--- src/test/java/GameOfLife/Test/GameTest.java
+++ src/test/java/GameOfLife/Test/GameTest.java
@@ -1,6 +1,5 @@
 package GameOfLife.Test;
 
-import GameOfLife.Board;
 import junit.framework.TestCase;
 import org.junit.Test;
 
@@ -10,9 +9,5 @@ public class GameTest extends TestCase {
     @Test
     public void testEqualValuePositions() throws Exception {
         assertEquals(at(0,1), at(0,1));
-    }
-    @Test
-    public void testEqualBoard() throws Exception {
-        assertEquals(new Board(at(0,1)), new Board(at(0,1)));
     }
 }
